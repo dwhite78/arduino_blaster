55f7c86f0d51acfc958d9086f1950b534d8d5088
David White <dwhite78@gmail.com>
2022-08-31 18:40:47 -0400

    Update

--- blaster.ino
+++ blaster.ino
@@ -1,11 +1,38 @@
 //Beginner project for a 3d printed blaster gun with lights and sound.
+#include "Arduino_SensorKit.h"
 
-void setup()
-{
-    
+void setup() {
+  Oled.begin();
+  Oled.setFlipMode(true);
 }
 
-void loop()
-{
-    
+void loop() {
+  //int random_value = random(0, 1023);   // create a random value
+
+  Oled.setFont(u8x8_font_chroma48medium8_r);
+  Oled.setCursor(0, 0);
+  Oled.print("I Like Big Butts");
+  delay(1000);
+
+  Oled.setFont(u8x8_font_chroma48medium8_r);
+  Oled.setCursor(0, 1);
+  Oled.print("And I can not");
+  delay(1000);
+
+  Oled.setFont(u8x8_font_chroma48medium8_r);
+  Oled.setCursor(0, 2);
+  Oled.print("lie, you other");
+  delay(1000);
+
+  Oled.setFont(u8x8_font_chroma48medium8_r);
+  Oled.setCursor(0, 3);
+  Oled.print("brothers");
+  delay(1000);
+
+  Oled.setFont(u8x8_font_chroma48medium8_r);
+  Oled.setCursor(0, 4);
+  Oled.print("Can't Deny");
+  delay(1000);
+
+  Oled.clearDisplay();
 }
